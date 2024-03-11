e29e60e8a2d7b891ba78159ed611080a653f695b
RiseGhost <jomiguelsa2mt@gmail.com>
2024-03-11 20:28:16 +0000

    User envoroment

--- services/InitUserEnv.js
+++ services/InitUserEnv.js
@@ -5,26 +5,21 @@ const port = args[0]
 const user_id = args[1]
 
 const command = "sudo ./services/InitUserEnv.sh " + user_id
-console.log("Comando -> " + command)
 
-exec(command, (error, stdout, stderr) => {
+const child = exec(command, (error, stdout, stderr) => {
     if (error) console.log(error)
     if (stderr) console.log(stderr)
     console.log(stdout)
     console.log("Criando ficheiros.....")
+    finish = true
 })
 
-/*
-const ws = new WebSocket.Server({port: port})
+const ws = new WebSocket.Server({ port: port })
 
 ws.on('connection', (ws) => {
-    console.log("User ID -> " + user_id)
-    exec("sudo ./InitUserEnv.sh " + user_id, (error, stdout, stderr) => {
-        if (error) console.log(error)
-        if (stderr) console.log(stderr)
-        console.log(stdout)
-        console.log("Criando ficheiros.....")
-        ws.send("Criado com sucesso")
+    ws.send("Criando ambiente")
+    child.on('exit', (code,signal) => {
+        ws.send("Ambiente finalizado.")
+        ws.close()
     })
 })
-*/
